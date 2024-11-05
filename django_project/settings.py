4789e44e9820372140770e54df2fe23d0618417d
benhduong <benhduong@gmail.com>
2024-11-05 15:16:09 -0800

    fix: settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,9 +130,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'chmod a+x build.sh
-
-
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
