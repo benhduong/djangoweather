47ee219c2202864404d1ecb69f6095731cc5b649
benhduong <benhduong@gmail.com>
2024-11-06 05:49:20 -0800

    moved whitenoise

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,12 +43,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Only use clickjacking protection in deployments because the Development Web View uses 
